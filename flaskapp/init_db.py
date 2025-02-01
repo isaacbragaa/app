58646ed67a37aeb2053f6c3deef573a6e1798238
Isaac Braga <31108956+isaacbragaa@users.noreply.github.com>
2025-02-01 11:17:22 -0300

    Update init_db.py

--- flaskapp/init_db.py
+++ flaskapp/init_db.py
@@ -1,9 +1,18 @@
 import sqlite3
 
-connection = sqlite3.connect('/app/database.db')
+def init_db():
+    conn = sqlite3.connect('/app/data/database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cadastros (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL,
+            telefone TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
-with open('/app/flaskapp/schema.sql') as f:
-    connection.executescript(f.read())
-
-connection.commit()
-connection.close()
+if __name__ == '__main__':
+    init_db()
